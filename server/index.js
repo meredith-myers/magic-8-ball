fcc3ff66ff8b376e8cd232c1daaeba9605b420dd
Meredith Myers <meredith@myers1.com>
2020-09-12 13:21:12 -0500

    Implemented POST request responses

--- server/index.js
+++ server/index.js
@@ -2,14 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const responses = ['Yes', 'No', 'Maybe', 'Why not?', 'What does your heart tell you?', 'Obviously'];
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).send('Hello World!');
 })
 
 app.post('/', function (req, res) {
-  res.send('Got a POST request')
+  const randomIndex = Math.floor(Math.random() * (responses.length));
+  res.status(200).send(responses[randomIndex]);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
+
+/*
+Additional Ideas
+store questions per user to list on site when next visiting
+render most popular questions on load
+*/
