5e90ff7a262c013900bbaaca9441f869ccac6875
Meredith Myers <meredith@myers1.com>
2020-09-12 13:42:46 -0500

    Skeleton React setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const axios = require('axios');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+    )
+  }
+}
+
+export default App;
