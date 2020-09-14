6368efb50858006fd4e1b26e05229d50c43060b0
Meredith Myers <meredith@myers1.com>
2020-09-14 14:56:05 -0500

    Added styling

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -33,29 +33,37 @@ class App extends React.Component {
     .catch((error) => {
       console.log(error);
     });
-    // alert('A question was submitted: ' + this.state.question);
 
     event.target.reset();
   }
 
   render() {
     return (
-      <div>
+      <div id="container">
         <div id="title">
           <h1>Magic 8 Ball</h1>
         </div>
         <form onSubmit={this.handleSubmit}>
-        <label>
-          Please Enter Your Question<br></br>
-          <input type="text" id="questionField" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      {this.state.question}<br></br>
-      {this.state.response}
+          <label>
+            Please Enter Your Question<br></br>
+            <input type="text" id="questionField" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <div id="ball">
+          <p id="question">{this.state.question}</p>
+          <br></br>
+          <p id="answer">{this.state.response}</p>
+        </div>
       </div>
     )
   }
 }
 
 export default App;
+
+/*
+Additional Ideas
+Store previously asked questions on page
+*/
